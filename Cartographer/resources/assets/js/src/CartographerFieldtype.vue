6cd08a6a4ce0124a74fcd8f93f4ddcec5ff111f6
Andrew Haine <andrew@andrewhaine.co.uk>
2019-06-07 00:27:17 +0100

    Add interface for creating additional markers

--- Cartographer/resources/assets/js/src/CartographerFieldtype.vue
+++ Cartographer/resources/assets/js/src/CartographerFieldtype.vue
@@ -11,33 +11,44 @@
 </template>
 
 <script>
+import uuid from "uuid/v4";
+
 export default {
   mixins: [Fieldtype],
 
   props: ["data", "config", "name"],
 
+  data() {
+    return {
+      map: null
+    };
+  },
+
   attached() {
     if (typeof google === "undefined") return;
 
     const mapEl = this.$els.mapContainer;
-    const defaultCoords = {
+
+    const center = {
       lat: this.data.center.lat || 52.6318051,
       lng: this.data.center.lng || 1.296734
     };
-    const map = new google.maps.Map(this.$els.mapContainer, {
-      center: defaultCoords,
+
+    this.map = new google.maps.Map(mapEl, {
+      center: center,
       fullscreenControl: false,
       mapTypeId: this.data.map_type_id || "roadmap",
       streetViewControl: false,
       zoom: 3
     });
 
-    map.addListener("maptypeid_changed", () => this.handleMapTypeChange(map));
+    this.map.addListener("maptypeid_changed", this.handleMapTypeChange);
 
     const centerMarker = new google.maps.Marker({
       draggable: true,
-      map: map,
-      position: defaultCoords
+      map: this.map,
+      position: center,
+      label: "C"
     });
 
     ["drag", "dragend"].forEach(eventType => {
@@ -45,15 +56,61 @@ export default {
         this.handleMarkerDragged(event, "center", true)
       );
     });
+
+    this.data.markers = this.data.markers || [];
+    this.data.markers.length && this.populateMarkers(this.data.markers);
   },
 
   methods: {
-    handleMapTypeChange(map) {
+    addMarker() {
+      const id = uuid();
+
+      const newMarker = new google.maps.Marker({
+        animation: google.maps.Animation.DROP,
+        draggable: true,
+        map: this.map,
+        position: this.map.getCenter(),
+        id
+      });
+
+      this.data.markers.push({
+        id,
+        position: this.map.getCenter().toJSON()
+      });
+
+      ["drag", "dragend"].forEach(eventType => {
+        newMarker.addListener(eventType, event =>
+          this.handleMarkerDragged(event, id)
+        );
+      });
+    },
+
+    populateMarkers(markers) {
+      markers.forEach((marker, i) => {
+        i++;
+        const markerObj = new google.maps.Marker({
+          draggable: true,
+          map: this.map,
+          position: marker.position,
+          id: marker.id,
+          label: i.toString()
+        });
+
+        ["drag", "dragend"].forEach(eventType => {
+          markerObj.addListener(eventType, event =>
+            this.handleMarkerDragged(event, markerObj.id)
+          );
+        });
+      });
+    },
+
+    handleMapTypeChange() {
       this.data = {
         ...this.data,
-        map_type_id: map.getMapTypeId()
+        map_type_id: this.map.getMapTypeId()
       };
     },
+
     handleMarkerDragged(event, markerId, isCenter = false) {
       const [lat, lng] = [event.latLng.lat(), event.latLng.lng()];
       if (isCenter) {
@@ -61,7 +118,23 @@ export default {
           ...this.data,
           center: { lat, lng }
         };
+      } else {
+        this.updateMarker(markerId, { position: { lat, lng } });
       }
+    },
+
+    updateMarker(markerId, data) {
+      const markers = this.data.markers;
+      const markerIndex = markers.findIndex(marker => marker.id === markerId);
+      if (markerIndex < 0) return false;
+      markers[markerIndex] = {
+        ...markers[markerIndex],
+        ...data
+      };
+      this.data = {
+        ...this.data,
+        markers
+      };
     }
   }
 };
