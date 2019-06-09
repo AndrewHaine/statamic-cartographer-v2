b4080f21b66f5ccd99d8df19995af45df7f8c2b6
Andrew Haine <andrew@andrewhaine.co.uk>
2019-06-09 18:52:16 +0100

    Add support for text-based location search using gmaps geocoder

--- Cartographer/CartographerFieldtype.php
+++ Cartographer/CartographerFieldtype.php
@@ -7,6 +7,19 @@ use Statamic\Extend\Fieldtype;
 class CartographerFieldtype extends Fieldtype
 {
 
-    public $category = ['special'];
+	public $category = ['special'];
+
+	public function preProcess($data)
+	{
+		$data['search_enabled'] = $this->getConfigBool('google_maps_geocoding_enabled', false);
+		$data['api_key'] = $this->getConfig('google_maps_api_key', '');
+		return $data;
+	}
+
+	public function process($data)
+	{
+		unset($data['search_enabled'], $data['api_key']);
+		return $data;
+	}
 
 }
--- Cartographer/resources/assets/js/src/CartographerFieldtype.vue
+++ Cartographer/resources/assets/js/src/CartographerFieldtype.vue
@@ -1,16 +1,32 @@
 <template>
   <section class="cartographer-field">
-    <div class="btn-group my-1 mr-2">
-      <a href="#" class="btn btn-default" @click.prevent="addMarker">
-        Marker
-        <i class="icon icon-plus icon-right"></i>
-      </a>
-    </div>
-    <div v-if="selectedMarker" class="btn-group my-1 mr-2">
-      <a href="#" class="btn btn-danger" @click.prevent="removeMarker(selectedMarker)">
-        Remove Marker
-        <i class="icon icon-trash icon-right"></i>
-      </a>
+    <div class="cartographer-field__controls flex justify-between items-center">
+      <div class="cartograpger-field__marker-controls">
+        <div v-if="dirtyCenter" class="btn-group my-1 mr-1">
+          <a href="#" class="btn btn-primary" @click.prevent="setCenter">Set center</a>
+        </div>
+        <div class="btn-group my-1 mr-2">
+          <a href="#" class="btn btn-default" @click.prevent="addMarker">
+            Marker
+            <i class="icon icon-plus icon-right"></i>
+          </a>
+        </div>
+        <div v-if="selectedMarker" class="btn-group my-1 mr-2">
+          <a href="#" class="btn btn-danger" @click.prevent="removeMarker(selectedMarker)">
+            Remove Marker
+            <i class="icon icon-trash icon-right"></i>
+          </a>
+        </div>
+      </div>
+      <div v-if="searchEnabled" class="controls flex items-center lg:w-auto">
+        <input
+          v-on:keyup.13="requestLocation"
+          type="text"
+          class="filter-control search w-full"
+          placeholder="Search for a location"
+          :disabled="isBusy"
+        >
+      </div>
     </div>
     <div class="cartographer-field__map" v-el:map-container></div>
   </section>
@@ -18,6 +34,7 @@
 
 <script>
 import uuid from "uuid/v4";
+import axios from "axios";
 
 export default {
   mixins: [Fieldtype],
@@ -26,17 +43,30 @@ export default {
 
   data() {
     return {
+      apiKey: "",
       center: {},
+      dirtyCenter: false,
+      busy: false,
       map: null,
       map_type_id: "roadmap",
       markers: [],
       markerObjects: [],
+      searchEnabled: false,
       selectedMarker: null
     };
   },
 
+  computed: {
+    isBusy() {
+      return this.busy;
+    }
+  },
+
   attached() {
     if (typeof google === "undefined") return;
+
+    this.apiKey = this.data.api_key;
+    this.searchEnabled = this.data.search_enabled;
 
     const mapEl = this.$els.mapContainer;
 
@@ -57,15 +87,21 @@ export default {
 
     this.map.addListener("maptypeid_changed", this.handleMapTypeChange);
 
-    const centerMarker = new google.maps.Marker({
+    this.map.addListener("dragend", () => (this.dirtyCenter = true));
+
+    this.centerMarker = new google.maps.Marker({
       draggable: true,
       map: this.map,
       position: this.center,
-      label: "C"
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        strokeColor: "red"
+      }
     });
 
     ["drag", "dragend"].forEach(eventType => {
-      centerMarker.addListener(eventType, event =>
+      this.centerMarker.addListener(eventType, event =>
         this.handleMarkerDragged(event, "center", true)
       );
     });
@@ -168,6 +204,36 @@ export default {
       } else {
         this.updateMarker(markerId, { position: { lat, lng } });
       }
+    },
+
+    setCenter() {
+      this.dirtyCenter = false;
+      this.centerMarker.setPosition(this.map.getCenter());
+      this.data.center = this.map.getCenter().toJSON();
+    },
+
+    requestLocation(e) {
+      this.busy = true;
+
+      const query = encodeURI(e.target.value);
+      const endpointURI = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${
+        this.apiKey
+      }`;
+
+      axios
+        .get(endpointURI)
+        .then(response => {
+          const result = response.data.results[0];
+          const { lat, lng } = result.geometry.location;
+          const center = new google.maps.LatLng(lat, lng);
+          this.map.panTo(center);
+          this.map.setZoom(14);
+          this.dirtyCenter = true;
+        })
+        .catch(e => console.error(e))
+        .finally(() => {
+          this.busy = false;
+        });
     }
   }
 };
